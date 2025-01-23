94b34652df1afbf9e58ac59dfe11d90c38de13ba
Harshvardhan Rijhwani <harshrijhwani4@gmail.com>
2025-01-23 20:36:03 +0530

    db connect and migration

---
+++ migration/src/lib.rs
@@ -0,0 +1,11 @@
+pub use sea_orm_migration::prelude::*;
+mod m20250123_150453_create_user_table;
+
+pub struct Migrator;
+
+#[async_trait::async_trait]
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        vec![Box::new(m20250123_150453_create_user_table::Migration)]
+    }
+}
